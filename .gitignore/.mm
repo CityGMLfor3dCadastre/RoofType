50008b78083cc6bb92d28b20de186c7a704a8f06
CityGMLfor3dCadastre <31859933+CityGMLfor3dCadastre@users.noreply.github.com>
2017-09-11 18:57:28 +0200

    Update and rename .gitignore to .mm

---
+++ .gitignore/.mm
@@ -0,0 +1,59 @@
+
+
+<!-- Lista kodova navodi vrijednosti atributa za "RoofType" (tip krova) klase "_AbstractBuilding" u modulu "building".
+
+Jednostavan rjecnik GML 3.1.1 se koristi za predstavljanje vrijednosti (element "ime") i (od strane covjeka za citanje), opisi (element "opis") ovih vrijednosti su u ovoj datoteci. Vrijednosti se koriste u primjerima dokumenata. -->
+<gml:Dictionary
+	xmlns:gml="http://www.opengis.net/gml"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/gml http://schemas.opengis.net/gml/3.1.1/profiles/SimpleDictionary/1.0.0/gmlSimpleDictionaryProfile.xsd" gml:id="_AbstractBuilding_function">
+	<gml:name>_AbstractBuilding_RoofType</gml:name>
+	<gml:dictionaryEntry>
+		<gml:Definition gml:id="id1">
+			<gml:description>ravan krov</gml:description>
+			<gml:name>11</gml:name>
+		</gml:Definition>
+	</gml:dictionaryEntry>
+	<gml:dictionaryEntry>
+		<gml:Definition gml:id="id2">
+			<gml:description>jednovodni</gml:description>
+			<gml:name>12</gml:name>
+		</gml:Definition>
+	</gml:dictionaryEntry>
+	<gml:dictionaryEntry>
+		<gml:Definition gml:id="id3">
+			<gml:description>dvovodni</gml:description>
+			<gml:name>13</gml:name>
+		</gml:Definition>
+	</gml:dictionaryEntry>
+	<gml:dictionaryEntry>
+		<gml:Definition gml:id="id4">
+			<gml:description>troovodni</gml:description>
+			<gml:name>14</gml:name>
+		</gml:Definition>
+	</gml:dictionaryEntry>
+	<gml:dictionaryEntry>
+		<gml:Definition gml:id="id5">
+			<gml:description>cetvorovodni</gml:description>
+			<gml:name>15</gml:name>
+		</gml:Definition>
+	</gml:dictionaryEntry>
+	<gml:dictionaryEntry>
+		<gml:Definition gml:id="id6">
+			<gml:description>složeni viševodni</gml:description>
+			<gml:name>16</gml:name>
+		</gml:Definition>
+	</gml:dictionaryEntry>
+	<gml:dictionaryEntry>
+		<gml:Definition gml:id="id7">
+			<gml:description>kupolasti</gml:description>
+			<gml:name>17</gml:name>
+		</gml:Definition>
+	</gml:dictionaryEntry>
+	<gml:dictionaryEntry>
+		<gml:Definition gml:id="id7">
+			<gml:description>drugi tip</gml:description>
+			<gml:name>18</gml:name>
+		</gml:Definition>
+	</gml:dictionaryEntry>
+</gml:Dictionary> 
+
